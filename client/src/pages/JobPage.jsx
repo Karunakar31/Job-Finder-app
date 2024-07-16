0a23bdf220d48c37188679103b630a92ba4e2b16
Karunakar31 <talatamkarun@gmail.com>
2024-07-16 16:54:41 +0530

    fewchanges

--- client/src/pages/JobPage.jsx
+++ client/src/pages/JobPage.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { Header } from "../components/Header";
-import { fetchJobById } from "../api/Job";
+import { fetchJobById, deleteJob } from "../api/Job";
 import { Toaster, toast } from 'react-hot-toast';
 import MoneyBill from '../assets/moneyBill.png';
 import Duration from '../assets/duration.png';
--- server/index.js
+++ server/index.js
@@ -33,12 +33,12 @@ app.get('/health', (req,res)=>{
     })
 })
 
-// app.use("*", (req, res) => {
-//     res.status(404).json({
-//         message: 'Endpoint not found',
-//         status: 'Error',
-//     });
-// });
+app.use("*", (req, res) => {
+    res.status(404).json({
+        message: 'Endpoint not found',
+        status: 'Error',
+    });
+});
 
 app.use(errorHandler);
 
